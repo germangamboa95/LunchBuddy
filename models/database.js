5c97cae58ddbfe30aaf822e88a676253d667d87b
German Gamboa <germangamboa95@gmail.com>
2018-05-04 00:04:06 -0400

    database file updated to work with heroku

--- models/database.js
+++ models/database.js
@@ -1,9 +1,13 @@
 var mysql      = require('mysql');
 var connection = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  database : 'lunchBuddy',
-  port: '3306'
+  host: "us-cdbr-iron-east-05.cleardb.net",
+
+  // Your username
+  user: "b1b33428f65f9b",
+
+  // Your password
+  password: "3ea11123",
+  database: "heroku_0420777eb6f6d22"
 });
 
 
