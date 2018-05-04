ddf71b5286f1bf0d737c5a6609d8762fefe70a7c
German Gamboa <germangamboa95@gmail.com>
2018-05-04 00:17:23 -0400

    added default user photo

--- schema.sql
+++ schema.sql
@@ -6,8 +6,12 @@ CREATE TABLE users(
     user_id INT NOT NULL AUTO_INCREMENT, 
     user_email VARCHAR(30) NOT NULL UNIQUE, 
     user_password VARCHAR(30),
+    user_image VARCHAR(100) NOT NULL DEFAULT './public/imgs/400x400.png',
+    
     PRIMARY KEY (user_id)
 );
+
+ALTER TABLE users AUTO_INCREMENT  = 1; 
 
 CREATE TABLE results(
     quiz_id INT NOT NULL AUTO_INCREMENT,
@@ -21,3 +25,4 @@ CREATE TABLE results(
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+ALTER TABLE results AUTO_INCREMENT  = 1; 
